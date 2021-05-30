98ff9b396617c73f8581ddbcdcb793d469585565
HNMouna <hadjnasrmouna@gmail.com>
2021-05-30 16:18:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import './Component/Profile/style.css';
 import React from "react";
-import MyFirstComponent from './project/test';
+
 import FullName from './Component/Profile/FullName';
 import ProfilePhoto from './Component/Profile/ProfilePhoto';
 import Address from './Component/Profile/Address';
@@ -15,7 +15,7 @@ function App() {
  <FullName />
  <Address />
 
-   <MyFirstComponent />
+ 
  </>
 
     </div>
